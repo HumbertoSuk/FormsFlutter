e7d34746c7b4e8fe01be1a3f2a9640701f63702b
July Suk <humbertoxd25@gmail.com>
2024-04-12 23:19:33 -0700

    Modificar custom widget

--- lib/presentation/widget/RegisterForm.dart
+++ lib/presentation/widget/RegisterForm.dart
@@ -1,14 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_forms/presentation/widget/input/custom_text_form_field.dart';
 
-class RegisterForm extends StatelessWidget {
+class RegisterForm extends StatefulWidget {
   const RegisterForm({super.key});
 
+  @override
+  State<RegisterForm> createState() => _RegisterFormState();
+}
+
+class _RegisterFormState extends State<RegisterForm> {
   @override
   Widget build(BuildContext context) {
     return Form(
         child: Column(
       children: [
+        const CustomTextFormField(),
+        const SizedBox(
+          height: 20,
+        ),
+        const CustomTextFormField(),
+        const SizedBox(
+          height: 20,
+        ),
         const CustomTextFormField(),
         const SizedBox(
           height: 20,
